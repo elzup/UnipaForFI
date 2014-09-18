629af31ae903e93b2c7ad7e436a7c1ca990a9426
elzzup <guild0105@gmail.com>
2014-09-19 01:44:38 +0900

    refactor table

--- UnipaForFI.user.js
+++ UnipaForFI.user.js
@@ -19,6 +19,7 @@
       cg: {
         complete: 0,
         count: 0,
+        now: 0,
         list: [
           'CGモデリングおよび演習',
           'CGレンダリングおよび演習',
@@ -29,6 +30,7 @@
       vs: {
         complete: 0,
         count: 0,
+        now: 0,
         list: [
           '画像処理',
           '画像処理演習',
@@ -41,6 +43,7 @@
       mi: { 
         complete: 0,
         count: 0,
+        now: 0,
         list: [
           'ヒューマンインタラクション及び演習',
           '人間情報システムおよび演習',
@@ -51,6 +54,7 @@
       wi: {
         complete: 0,
         count: 0,
+        now: 0,
         list: [
           'サーバ設計論',
           '情報アクセスと知的処理',
@@ -62,6 +66,7 @@
       sn: {
         complete: 0,
         count: 0,
+        now: 0,
         list: [
           '情報セキュリティの基礎と暗号技術',
           'ネットワークプログラミング',
@@ -72,6 +77,7 @@
       st: {
         complete: 0,
         count: 0,
+        now: 0,
         list: [
           'サーバ設計論',
           'メディア情報学',
@@ -92,11 +98,11 @@
     // 単位認定可能な評価範囲
     var hyoka = ["S","A","B","C","RS","RA","RB","RC","RN"];
 
-    for (var unitKey in units){
+    for (var unitKey in units) {
       var unit = units[unitKey];
       var unitList = unit.list;
       unitSize = myUnitList.size();
-      for (var j=0; j < unitSize; ++j){
+      for (var j=0; j < unitSize; ++j) {
         var myUnitText = myUnitList[j].textContent;
         var myHyokaText = hyokaList[j].textContent;
         var i = unitList.indexOf(myUnitText);
@@ -111,102 +117,39 @@
           } 
         }
       }
+      unit.now = unit.count - unit.complete;
     }
-    console.log(units);
 
     // 要素の追加
     var table = $("table.outline tbody")[0];
     // unipaは本当に最悪なのでtdを追加しなくちゃレイアウトが崩れる
     var titleText = "<tr><td></td><td class='subTitleArea'>ユニット達成度</td><td></td><tr>";
-    var tableText = "\
-    <tr>\
-    <td></td>\
-    <td>\
-    <table class='singleTableLine'>\
-    <tbody>\
-    <tr>\
-    <th class='headTaniShutoku'>ユニット分類</th>\
-    <td class='dataTaniShutokuKamoku'>CG</th>\
-    <td class='dataTaniShutokuKamoku'>VS</th>\
-    <td class='dataTaniShutokuKamoku'>MI</th>\
-    <td class='dataTaniShutokuKamoku'>WI</th>\
-    <td class='dataTaniShutokuKamoku'>ST</th>\
-    <td class='dataTaniShutokuKamoku'>SN</th>\
-    </tr>\
-    <tr>\
-    <th class='headTaniShutoku'>履修済単位</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + units.cg.complete
-    + "</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + units.vs.complete
-    + "</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + units.mi.complete
-    + "</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + units.wi.complete
-    + "</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + units.st.complete
-    + "</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + units.sn.complete
-    + "</th>\
-    </tr>\
-    <tr>\
-    <th class='headTaniShutoku'>履修中単位</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + (units.cg.count - units.cg.complete)
-    + "</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + (units.vs.count - units.vs.complete)
-    + "</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + (units.mi.count - units.mi.complete)
-    + "</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + (units.wi.count - units.wi.complete)
-    + "</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + (units.st.count - units.st.complete)
-    + "</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + (units.sn.count - units.sn.complete)
-    + "</th>\
-    </tr>\
-    <tr>\
-    <th class='headTaniShutoku'>ユニット合計</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + units.cg.count
-    + "</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + units.vs.count
-    + "</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + units.mi.count
-    + "</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + units.wi.count
-    + "</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + units.st.count
-    + "</th>\
-    <td class='dataTaniShutokuKamoku'>"
-    + units.sn.count
-    + "</th>\
-    </tr>\
-    </tbody>\
-    </table>\
-    </td>\
-    <td></td>\
-    </tr>\
-    ";
+    var tablePrefix = '<tr><td></td><td>';
+    var tableText = "<table class='singleTableLine'>";
+
+    var tableParams = [
+        ['ユニット分類', '"unitKey"'],
+        ['履修済単位', '"units." + unitKey + ".complete"'],
+        ['履修中単位', '"units." + unitKey + ".now"'],
+        ['ユニット合計', '"units." + unitKey + ".count"']
+    ];
+
+    for (var i = 0; i < tableParams.length; i++) {
+        tableText += "<tr>";
+        tableText += "<td class='headTaniShutoku'>" + tableParams[i][0];
+        for (var unitKey in units) {
+            tableText += "<td class='dataTaniShutokuKamoku'>";
+            tableText += eval(eval(tableParams[i][1]));
+        }
+    }
+
+    tableText += "</table>";
+    var tableSuffix = '</td><td></td></tr>';
 
     var link = "<tr><td></td><td><a href='http://www.im.dendai.ac.jp/curriculum.html' target='_blank'>ユニット詳細(学科ページ)</a></td></tr>"
 
     $(table).append(titleText);
-    $(table).append(tableText);
+    $(table).append(tablePrefix + tableText + tableSuffix);
     $(table).append(link);
     
     // 動作確認
